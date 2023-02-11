eaab62e59985d48704c042f70494e2485b5f8d46
Teodora Vodopija <115836182+TeodoraVodopija@users.noreply.github.com>
2023-02-11 12:59:17 +0100

    receiving data, timestamps

--- master.py
+++ master.py
@@ -5,9 +5,6 @@ import random
 import time
 from aiohttp import web
 
-max_number_of_workers = 10
-start_time = time.monotonic_ns()
-
 routes = web.RouteTableDef()
 
 @routes.post("/data_send")
@@ -51,18 +48,16 @@ async def receive_data(request):
                             start_time = time.monotonic_ns()  #start a timer for each task
                             task = asyncio.create_task(session.post(
                                 f"http://localhost:{port}/receive_data",
-                                json = {"data": data_to_send, "worker_id": worker_id}))  #create task to send data
+                                json = {"Data": data_to_send, "Worker ID": worker_id}))  #create task to send data
                             tasks.append(task)
                     await asyncio.gather(*tasks)
                     for port in task_count:
                         print(f"Total tasks sent to worker{port}: {task_count[port]}")
                     lines = lines[3:]
-        else:
-            print("No workers are ready.")
+        else: print("No workers are ready.")
 
         #if all data has been sent, print a message
-        if not lines:
-            print("No more data to send.")
+        if not lines: print("No more data to send.")
 
     #return a response to the client indicating that the data has been received
     return web.Response(text = "Data received.")
@@ -70,32 +65,28 @@ async def receive_data(request):
 #define a dictionary to store the word counts received from the worker nodes
 word_counts = {}
 
-@routes.post("/receive_worker_word_count")
+@routes.post("/receive_word_count")
 async def receive_word_count(request):
     #receive the word count data from the worker
     data = await request.json()
     #extract the worker port and word count from the received data
-    worker_port = data["worker_port"]
-    word_count = data["word_count"]
+    worker_port = data["Worker port"]
+    word_count = data["Number of words"]
 
-    # Log the current time when receiving the word count from the worker
+    #log the current time when receiving the word count from the worker
     current_time = time.monotonic_ns()  #timer ends here
     print(f"Received word count from {worker_port} : {word_count} words counted.")
-    elapsed_time = current_time - start_time  #calculate the elapsed time
-    # print(f"Elapsed time between sending and receiving data: {elapsed_time} ns")
+    elapsed_time = current_time - (time.monotonic_ns())  #calculate the elapsed time
     print(f"Elapsed time between sending and receiving data: {elapsed_time} ns ({elapsed_time / 1000000:.2f} ms)")
 
     #increment the count of completed tasks from the worker
-    if worker_port in word_counts:
-        word_counts[worker_port] += 1
-    else:
-        word_counts[worker_port] = 1
+    if worker_port in word_counts: word_counts[worker_port] += 1
+    else: word_counts[worker_port] = 1
 
     #print the total number of completed tasks from the worker
     print(f"Total tasks returned {worker_port}: {word_counts[worker_port]}")
 
     return web.Response(status = 200)
-
 
 app = web.Application(client_max_size = 1024 * 1024 * 200)
 app.add_routes(routes)
