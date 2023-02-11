81664fd465c328d1cab7e5ea4f613ac92b99aa66
Teodora Vodopija <115836182+TeodoraVodopija@users.noreply.github.com>
2023-02-11 12:50:34 +0100

    workers - word counters

--- worker3.py
+++ worker3.py
@@ -1,30 +1,47 @@
 #import packages (constraints)
-from aiohttp import web
-import random
 import asyncio
+import aiohttp
+from aiohttp import web
 import re
 import string
+import random
+import logging
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(level = logging.INFO)
 
 routes = web.RouteTableDef()
 
-@routes.get("/")
-async def word_counter(request):
-	try:
-		#network delay 0.1 - 0.3 s (simulation)
-		await asyncio.sleep(random.uniform(0.1, 0.3))
+@routes.get("/worker_ready")
+async def worker_ready(request):
+    return web.Response(status = 200)
 
-		#counter of words
-		jsonData = await request.json()
-		result = len(re.sub("[" + string.punctuation + "]", "", jsonData.get("data")).split())
+@routes.post("/receive_data")
+async def receive_data(request):
+    #network delay 0.1 - 0.3 s (simulation)
+    await asyncio.sleep(random.uniform(0.1, 0.3))
 
-		#network delay 0.1 - 0.3 s (simulation)
-		await asyncio.sleep(random.uniform(0.1, 0.3))
+    #receiving codes from master
+    data = await request.json()
 
-		return web.json_response({"Name": "worker3", "Status": "OK", "Number of words": result}, status = 200)
+    #count the number of words in the file
+    async def count_words(text: str) -> int:
+        await asyncio.sleep(0)  #simulate an async operation
+        return len(re.sub("[" + string.punctuation + "]", "", str(text)).split())
 
-	except Exception as ex:
-		return web.json_response({"Name": "worker3", "Error": str(ex)}, status = 500)
+    number_of_words = await count_words(data)
+    logging.info(f"Word count: {number_of_words}")
 
-app = web.Application()
-app.router.add_routes(routes)
+    #send the number of words back to master
+    async with aiohttp.ClientSession() as session:
+        #network delay 0.1 - 0.3 s (simulation)
+        await asyncio.sleep(random.uniform(0.1, 0.3))
+        await session.post("http://localhost:8080/receive_word_count",
+                            json = {"Worker port": "worker" + str(request.transport.get_extra_info("sockname")[1]),
+                                    "Number of words": number_of_words})
+
+    return web.Response(status = 200)
+
+app = web.Application(client_max_size = 1024 * 1024 * 200)
+app.add_routes(routes)
 web.run_app(app, port = 8083)
